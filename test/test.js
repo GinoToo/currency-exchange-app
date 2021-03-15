9b5670a6c211bd91f5a73f3f894da4c663bce6c5
gino too <ginotoo@monogold.com>
2021-03-15 11:19:43 +0800

    added circle docker

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 // test/test.js
 const { expect } = require("chai")
 
-const serverURL = "http://127.0.0.1:8000"
+const serverURL = "http://127.0.0.1:3000"
 const request = require("supertest")(serverURL)
 
 describe("Test", function () {
@@ -10,14 +10,14 @@ describe("Test", function () {
   })
 })
 
-// describe("amount with specific currency, and show converted values in all other currencies", function () {
-//   it("Should return crrency as number", async function () {
-//     const response = await request.get("/conversion?base=usd&amount=100")
-//     expect(response.status).to.eql(200)
-//     let data = response.body
-//     expect(data["CAD"]).to.be.a("number")
-//   })
-// })
+describe("amount with specific currency, and show converted values in all other currencies", function () {
+  it("Should return crrency as number", async function () {
+    const response = await request.get("/conversion?base=usd&amount=100")
+    expect(response.status).to.eql(200)
+    let data = response.body
+    expect(data["CAD"]).to.be.a("number")
+  })
+})
 
 // describe("get historical exchange rates", function () {
 //   it("Should return date key and crrency as number", async function () {

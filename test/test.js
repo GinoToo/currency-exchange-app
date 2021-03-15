2ccbeaa388df57b2c06ee819352dac84d74d60c4
gino too <ginotoo@monogold.com>
2021-03-15 14:20:11 +0800

    fix circle with test

--- test/test.js
+++ test/test.js
@@ -4,12 +4,6 @@ const { expect } = require("chai")
 const serverURL = "http://127.0.0.1:3000"
 const request = require("supertest")(serverURL)
 
-describe("Test", function () {
-  it("should return true", function () {
-    expect(123).to.be.a("number")
-  })
-})
-
 describe("amount with specific currency, and show converted values in all other currencies", function () {
   it("Should return crrency as number", async function () {
     const response = await request.get("/conversion?base=usd&amount=100")
@@ -19,13 +13,13 @@ describe("amount with specific currency, and show converted values in all other 
   })
 })
 
-// describe("get historical exchange rates", function () {
-//   it("Should return date key and crrency as number", async function () {
-//     const response = await request.get(
-//       "/history?from=2021-01-01&to=2021-01-12&base=USD"
-//     )
-//     expect(response.status).to.eql(200)
-//     let data = response.body
-//     expect(data["2021-01-08"]["CAD"]).to.be.a("number")
-//   })
-// })
+describe("get historical exchange rates", function () {
+  it("Should return date key and crrency as number", async function () {
+    const response = await request.get(
+      "/history?from=2021-01-01&to=2021-01-12&base=USD"
+    )
+    expect(response.status).to.eql(200)
+    let data = response.body
+    expect(data["2021-01-08"]["CAD"]).to.be.a("number")
+  })
+})
